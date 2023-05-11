af66f1fb76c494ff3af6404b872fb9ff730c1d4b
Ivan Chupshev <64410743+KuRamma69@users.noreply.github.com>
2023-05-11 20:00:16 +0300

    Была создана страница newspage и slider на странитце main.php

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+   $('.slider').slick({
+      arrows: false,
+      dots: true,
+      adaptiveHeight: true,
+      slidesToShow: 3,
+      slidesToScroll: 2,
+      speed: 800,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      draggble: true,
+      touchThreshold: 10,
+      waitForAnimate: true, /*быстрая прокрутка вручную */
+      centerMode: false,
+      variableWidth: true,
+
+   });
+});
+
